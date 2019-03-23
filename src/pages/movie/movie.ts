8bde6024a30b95743b82ead4437781f3f48aeeb5
nitipong santiphapchai <nitipong.za1997@gmail.com>
2019-03-23 11:37:02 +0700

    movie

--- src/pages/movie/movie.ts
+++ src/pages/movie/movie.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 /**
  * Generated class for the MoviePage page.
  *
@@ -14,8 +15,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'movie.html',
 })
 export class MoviePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  movie:any=0;
+    constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
+      this.http.get('https://www.reddit.com/r/movies/new.json?limt=20')
+        .map(res=>res.json())
+        .subscribe(data=>{
+          this.movie = data.data.children;
+  });
   }
 
   ionViewDidLoad() {
